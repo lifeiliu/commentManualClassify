1e17c806b097e3fb2b10308660ea3c28510f56da
Lifei Liu <lliu.lifei@gmail.com>
2018-12-28 08:56:00 -0600

    finished 2 class, needs to test

--- src/CommentForCat.java
+++ src/CommentForCat.java
@@ -21,29 +21,33 @@ public class CommentForCat {
         this.fileName = fileName;
     }
 
-    private CommentForCat(String text, String commentedCode, String methodName,
-                          String className, int lineStartNumber, int lineEndNumber) {
+    private CommentForCat(String text, String commentedCode,CommentLocation location,
+                          int lineStartNumber, int lineEndNumber,CommentType type) {
         this.text = text;
         this.commentedCode = commentedCode;
-        this.methodName = methodName;
-        this.className = className;
         this.lineStartNumber = lineStartNumber;
         this.lineEndNumber = lineEndNumber;
+        this.commentLocation = location;
+        this.commentType = type;
     }
 
-    public CommentForCat convertFromComment(Comment comment){
+    public static CommentForCat convertFromComment(Comment comment){
         String text = comment.getContent();
         CommentType commentType = getCommentType(comment);
         CommentLocation commentLocation = getCommentLocation(comment);
+        int lineStartNumber = getLineStartNumber(comment);
+        int lineEndNumber = getLineEndNumber(comment);
+        String commentedCode = getCommentedCode(comment);
+        return new CommentForCat(text,commentedCode,commentLocation,lineStartNumber,lineEndNumber,commentType);
 
     }
 
-    private CommentType getCommentType(Comment comment){
+    private static CommentType getCommentType(Comment comment){
         if (comment.isJavadocComment()) return CommentType.DocumentComment;
         else return CommentType.RegularComment;
     }
 
-    private CommentLocation getCommentLocation(Comment comment){
+    private static CommentLocation getCommentLocation(Comment comment){
         if(!comment.getCommentedNode().isPresent()) return  CommentLocation.OrphanComment;
         Node node = comment.getCommentedNode().get();
         if (node instanceof ClassOrInterfaceDeclaration) return CommentLocation.ClassTopComment;
@@ -63,6 +67,18 @@ public class CommentForCat {
 
     }
 
+    private static int getLineStartNumber(Comment comment){
+        return comment.getRange().get().begin.line;
+    }
+    private static int getLineEndNumber(Comment comment){
+        return comment.getRange().get().end.line;
+    }
+
+    private static String getCommentedCode(Comment comment){
+        if (! comment.getCommentedNode().isPresent())
+            return null;
+        return comment.getCommentedNode().get().removeComment().toString();
+    }
 
 
 }
--- src/SourceFileUtil.java
+++ src/SourceFileUtil.java
@@ -11,18 +11,19 @@ public class SourceFileUtil {
 
 
     public static List<CommentForCat> getCommentsFromFile(File sourceCodeFile) throws Exception {
-
+        List<CommentForCat> result = new LinkedList<>();
         if (!verifySourceCode(sourceCodeFile)) throw new Exception("not java file");
         try {
             CompilationUnit cu = JavaParser.parse(sourceCodeFile);
             List<Comment> comments = cu.getComments();
-
-
+            List<Comment> combinedComments = combineMultiLineComment(comments);
+            for (Comment comment : combinedComments){
+                result.add(CommentForCat.convertFromComment(comment));
+            }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-
-
+        return result;
 
     }
 
@@ -32,5 +33,17 @@ public class SourceFileUtil {
             return "java".equals(fileName.substring(fileName.lastIndexOf("." + 1)));
         }
         return false;
+    }
+
+    private static List combineMultiLineComment (List<Comment> comments){
+        for (int i = 0; i < comments.size() - 1; i++){
+
+            if (comments.get(i).isOrphan() && comments.get(i).getEnd().get().line == comments.get(i+1).getBegin().get().line - 1 ) {
+                comments.get(i + 1).setContent(comments.get(i).getContent() + comments.get(i+1).getContent());
+                comments.remove(i);
+                i--;
+            }
+        }
+        return comments;
     }
 }
